530009d0b2424dc694fee7353a32a4e85c09efce
TheMisterCrunch <94947915+TheMisterCrunch@users.noreply.github.com>
2024-10-30 15:07:49 +0100

    changement

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize')
 
 // Database
 const sequelize = new Sequelize(
-  '', // TODO: database connection string
+  'postgresql://cloud_db_9e0d_user:Ao7AreJ0Gpa4cErr04Y9ZtnR4lV6uGGr@dpg-csh3jtrqf0us739tqh80-a/cloud_db_9e0d',  // TODO: database connection string
   {
     dialect: 'postgres',
     dialectOptions: {
