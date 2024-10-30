1ddd00831c0e3e43a005920e1dda4f954f36eb32
TheMisterCrunch <94947915+TheMisterCrunch@users.noreply.github.com>
2024-10-30 15:18:01 +0100

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize')
 
 // Database
 const sequelize = new Sequelize(
-  'postgresql://cloud_db_9e0d_user:Ao7AreJ0Gpa4cErr04Y9ZtnR4lV6uGGr@dpg-csh3jtrqf0us739tqh80-a/cloud_db_9e0d',  // TODO: database connection string
+  process.env.DATABASE_URL,
   {
     dialect: 'postgres',
     dialectOptions: {
